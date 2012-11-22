12003c2265f5e96edbd687837b5e98bb892f65dd
Mark Percival <mark@markpercival.us>
2012-11-22 16:41:56 +0000

    Update README, fix maxRounds

--- README.md
+++ README.md
@@ -5,8 +5,25 @@
     npm install # Install the neccessary libs
     node tourny.js
 
-## Config
+## Holiday Poker Tournament
 
 Currently we are running https://github.com/mdp/HolidayPoker/blob/master/tourny.js
 
-Check out https://github.com/mdp/HolidayPoker/blob/master/examples/tournament.js for more info and setup options
+Join the tournament by sending a pull request and adding yourself to the list.
+
+Check out the Google Group for more info - https://groups.google.com/forum/?fromgroups#!forum/machine-poker
+
+### Config for development
+
+Check out https://github.com/mdp/HolidayPoker/blob/master/examples/tournament.js for more info
+and setup options. You can build your own tournament using this as a template.
+
+### Build your bot
+
+Check [this guide on the wiki](https://github.com/mdp/MachinePoker/wiki) to start building your own bot
+
+## Dependencies
+
+- machine-poker >= 0.3.0
+- nodejs >= 0.8.0
+
--- tourny.js
+++ tourny.js
@@ -5,15 +5,27 @@ var dateStr = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate
 var fileLogger = MachinePoker.observers.fileLogger('./results/results-'+ dateStr +'.json');
 
 var table = MachinePoker.create({
-  "maxRounds" : 2,
+  "maxRounds" : 1000,
   "chips" : 1000,
   "betting" : MachinePoker.betting.noLimit(10,20)
 });
 
-table.addPlayer("https://raw.github.com/gist/d2d69b3178379458d2b9/HardlyKnower.js", {name: "HardlyKnower - @farktronix"});
-table.addPlayer("https://raw.github.com/gist/affa85924b3b9ba115dd/rpj-losing_statelessly.js", {name: "Losing Statelessly - @rpj"});
-table.addPlayer("https://raw.github.com/gist/7ea74e5aca4da2265382/bot.js", {name: "Rucky Bot - @chenosaurus"});
-table.addPlayer("https://raw.github.com/gist/ee24fb0ebcf00fbc9f23/jacksorbetterbot.js", {name: "Jacks Or Better - @kylar42"});
+table.addPlayer(
+  "https://raw.github.com/gist/d2d69b3178379458d2b9/HardlyKnower.js",
+  {name: "HardlyKnower - @farktronix"}
+);
+table.addPlayer(
+  "https://raw.github.com/gist/affa85924b3b9ba115dd/rpj-losing_statelessly.js",
+  {name: "Losing Statelessly - @rpj"}
+);
+table.addPlayer(
+  "https://raw.github.com/gist/7ea74e5aca4da2265382/bot.js",
+  {name: "Rucky Bot - @chenosaurus"}
+);
+table.addPlayer(
+  "https://raw.github.com/gist/ee24fb0ebcf00fbc9f23/jacksorbetterbot.js",
+  {name: "Jacks Or Better - @kylar42"}
+);
 
 // Add some observers
 table.addObserver(narrator);
